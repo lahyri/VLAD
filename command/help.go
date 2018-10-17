05ec7a8df3b1e9db492f57d3ffa9a76869a4e285
lahyri <lahyri@tracksale.co>
2018-10-17 13:35:05 -0300

    adjusted help command

--- command/help.go
+++ command/help.go
@@ -1,16 +1,29 @@
 package command
 
 import (
+	"bufio"
+	"log"
+	"os"
+
 	"github.com/bwmarrin/discordgo"
 )
 
-const (
-	BotDescr         = ""
-	VampireRollDescr = "blabla"
-	DnDRollDescr     = "loblob"
-)
-
 func Help(s *discordgo.Session, m *discordgo.MessageCreate) {
-	s.ChannelMessageSend(m.ChannelID, VampireRollDescr)
+	path, _ := os.Getwd()
+	path += "/info/help.txt"
 
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		s.ChannelMessageSend(m.ChannelID, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
