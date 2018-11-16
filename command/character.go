6f22a26e328d72008a8398eaa64f2b97b764ec02
lahyri <lahyri@tracksale.co>
2018-11-16 09:39:09 -0200

    minor env adjusts

--- command/character.go
+++ command/character.go
@@ -1,16 +1,20 @@
 package command
 
 import (
-	"ioutil"
-	"encoding/json"
+	// "io/ioutil"
+	// "encoding/json"
 	"github.com/bwmarrin/discordgo"
+	// "github.com/lahyri/VLAD/character"
 )
 
+
+
 func NewVampire (name string, s *discordgo.Session, m *discordgo.MessageCreate){
-	vamp := character.VampireCharacter{
-		Name: name
-	}
-	filename := name + ".json"
-	character, err := json.Marshal(vamp)
-	err = ioutil.WriteFile(filename, character, 0777)
+	// vamp := character.VampireCharacter{
+	// 	Name: name,
+	// }
+	// filename := name + ".json"
+	// character, _ := json.Marshal(vamp)
+	// err := ioutil.WriteFile(filename, character, 0777)
+
 }
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"os/signal"
 	"syscall"
